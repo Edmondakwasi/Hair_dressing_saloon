249ea2f53a3eb2140e483a61c506d794ee2fc8c6
edmond akwasi <edmondakwasi133@gmail.com>
2024-10-30 12:27:34 +0000

    added a payment system

--- scripts/script.js
+++ scripts/script.js
@@ -1,6 +1,9 @@
-let navbar = document.querySelector('.header .navbar');
-let menuBtn = document.querySelector('#menu-btn');
-let closeBtn = document.querySelector('#close-navbar');
+const navbar = document.querySelector('.header .navbar');
+const menuBtn = document.querySelector('#menu-btn');
+const closeBtn = document.querySelector('#close-navbar');
+
+const privacyCheckbox = document.getElementById('privacyPolicy');
+const bookButton = document.getElementById('bookButton');
 
 menuBtn.onclick = () =>{
    navbar.classList.add('active');
@@ -13,3 +16,18 @@ closeBtn.onclick = () =>{
 window.onscroll = () =>{
    navbar.classList.remove('active');
 };
+
+// Enable button when checkbox is checked
+privacyCheckbox.addEventListener('change', function() {
+   if (this.checked) {
+      bookButton.style.pointerEvents = 'auto';
+         bookButton.style.opacity = '1';
+      } else {
+         bookButton.style.pointerEvents = 'none';
+         bookButton.style.opacity = '0.5';
+      }
+   });
+
+   function enableBooking() {
+      return privacyCheckbox.checked;
+}
