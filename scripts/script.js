4c4bd97673c2b04efee92f528514b8849d86b085
edmond akwasi <edmondakwasi133@gmail.com>
2025-01-04 22:18:50 +0000

    page update

--- scripts/script.js
+++ scripts/script.js
@@ -2,6 +2,9 @@ let navbar = document.querySelector('.header .navbar');
 let menuBtn = document.querySelector('#menu-btn');
 let closeBtn = document.querySelector('#close-navbar');
 
+const termsCheckbox = document.getElementById('accept-terms');
+const bookBtn = document.getElementById('book-btn');
+
 menuBtn.onclick = () =>{
    navbar.classList.add('active');
 };
@@ -13,3 +16,14 @@ closeBtn.onclick = () =>{
 window.onscroll = () =>{
    navbar.classList.remove('active');
 };
+
+//accept terms and conditions.
+    termsCheckbox.addEventListener('change', function() {
+        if (this.checked) {
+            bookBtn.style.pointerEvents = 'auto';  // Enable the button
+            bookBtn.style.opacity = '1';           // Make the button fully visible
+        } else {
+            bookBtn.style.pointerEvents = 'none';  // Disable the button
+            bookBtn.style.opacity = '0.5';         // Make the button appear disabled
+        }
+    });
